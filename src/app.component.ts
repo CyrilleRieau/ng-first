64b6ddd09387ddd96d56b91782396583bf489d8a
Cyrille <cyrille.rieau@gmail.com>
2017-09-19 11:04:01 +0200

    Commit

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,8 +1,13 @@
 import {Component} from '@angular/core';
 @Component ({
 selector : 'mon-app',
-template:'<h1>{{message}}</h1>'
+//template:'<h1>{{message}}</h1>'
+templateUrl:'./app.component.html'
 })
 export class AppComponent {
-    private message:string = 'un truc dedans';
+    private message:string = 'chose';
+    title='app';
+    methode() {
+        this.title='autre chose';
+        }
 }
